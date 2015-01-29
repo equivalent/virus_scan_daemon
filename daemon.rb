f3ae60deba5f6b3982f3d0466d17270e7b385d2e
Tomas Valent <equivalent@eq8.eu>
2015-01-29 11:34:28 +0000

    make script daemon

--- daemon.rb
+++ daemon.rb
@@ -5,27 +5,32 @@ require 'pathname'
 
 all_secrets = YAML.load(File.read('secrets.yml'))
 
-secrets = all_secrets.fetch(:staging)
 
 logger = Logger.new('virus_scan_service.log')
 logger.level = Logger::DEBUG
 
-courier = VirusScanService::Courier.new host: secrets.fetch(:host), token: secrets.fetch(:token)
-courier.logger = logger
-courier.call do |file_url|
-  kaspersky = VirusScanService::KasperskyRunner.new(file_url)
-  kaspersky.scan_log_path = Pathname.new('.').join('kaspersky.log')
+loop do
+  all_secrets.first(1).each do |secret_key, secrets|
+    logger.info = "Run for #{secret_key}"
+    courier = VirusScanService::Courier.new host: secrets.fetch(:host), token: secrets.fetch(:token)
+    courier.logger = logger
+    courier.call do |file_url|
+      kaspersky = VirusScanService::KasperskyRunner.new(file_url)
+      kaspersky.scan_log_path = Pathname.new('.').join('kaspersky.log')
 
-  kaspersky.scan_folder = Pathname
-    .new('.')
-    .join('scan_queue')
-    .tap { |path| FileUtils.mkdir_p(path) }
+      kaspersky.scan_folder = Pathname
+        .new('.')
+        .join('scan_queue')
+        .tap { |path| FileUtils.mkdir_p(path) }
 
-  kaspersky.archive_folder = Pathname
-    .new('.')
-    .join('scan_logs_archive')
-    .tap { |path| FileUtils.mkdir_p(path) }
+      kaspersky.archive_folder = Pathname
+        .new('.')
+        .join('scan_logs_archive')
+        .tap { |path| FileUtils.mkdir_p(path) }
 
-  kaspersky.call
-  kaspersky.result # return result to PUT request (E.g: Clean)
+      kaspersky.call
+      kaspersky.result # return result to PUT request (E.g: Clean)
+    end
+  end
+  sleep 2
 end
