553e19ccb7d19591a1e38724696c21087a2ac7db
Tomas Valent <equivalent@eq8.eu>
2015-02-03 11:44:15 +0000

    log all exceptions

--- daemon.rb
+++ daemon.rb
@@ -10,30 +10,34 @@ logger = Logger.new('virus_scan_service.log')
 logger.level = Logger::DEBUG
 
 loop do
-  all_secrets.first(1).each do |secret_key, secrets|
-    logger.info "Run for #{secret_key}"
-    courier = VirusScanService::Courier.new host: secrets.fetch(:host), token: secrets.fetch(:token)
-    courier.logger = logger
-    courier.call do |file_url|
-      kaspersky = VirusScanService::KasperskyRunner.new(file_url)
-      kaspersky.scan_log_path = Pathname.new('.').join('kaspersky.log')
+  begin
+    all_secrets.first(1).each do |secret_key, secrets|
+      logger.info "Run for #{secret_key}"
+      courier = VirusScanService::Courier.new host: secrets.fetch(:host), token: secrets.fetch(:token)
+      courier.logger = logger
+      courier.call do |file_url|
+        kaspersky = VirusScanService::KasperskyRunner.new(file_url)
+        kaspersky.scan_log_path = Pathname.new('.').join('kaspersky.log')
 
-      kaspersky.scan_folder = Pathname
-        .new('.')
-        .join('scan_queue')
-        .tap { |path| FileUtils.mkdir_p(path) }
+        kaspersky.scan_folder = Pathname
+          .new('.')
+          .join('scan_queue')
+          .tap { |path| FileUtils.mkdir_p(path) }
 
-      kaspersky.archive_folder = Pathname
-        .new('.')
-        .join('scan_logs_archive')
-        .tap { |path| FileUtils.mkdir_p(path) }
+        kaspersky.archive_folder = Pathname
+          .new('.')
+          .join('scan_logs_archive')
+          .tap { |path| FileUtils.mkdir_p(path) }
 
+        kaspersky.antivirus_exec = VirusScanService::KasperskyRunner::WindowsExecutor.new
 
-      kaspersky.antivirus_exec = VirusScanService::KasperskyRunner::WindowsExecutor.new
-
-      kaspersky.call
-      kaspersky.result # return result to PUT request (E.g: Clean)
+        kaspersky.call
+        kaspersky.result # return result to PUT request (E.g: Clean)
+      end
     end
+    sleep 2
+  rescue => e
+    logger.info e.to_s
+    sleep 60
   end
-  sleep 2
 end
