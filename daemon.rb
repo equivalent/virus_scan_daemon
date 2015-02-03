875ee676b5995bd587ecaef5277da9ec42170a1b
Tomas Valent <equivalent@eq8.eu>
2015-02-03 16:55:41 +0000

    logger level + bump service version

--- daemon.rb
+++ daemon.rb
@@ -5,13 +5,14 @@ require 'pathname'
 
 all_secrets = YAML.load(File.read('secrets.yml'))
 
-
-logger = Logger.new('virus_scan_service.log')
-logger.level = Logger::DEBUG
-
 loop do
+  logger = Logger.new('virus_scan_service.log')
+  logger.level = Logger::ERROR
+  logger.level = Logger::INFO  if File.exist?('info')
+  logger.level = Logger::DEBUG if File.exist?('debug')
+
   begin
-    all_secrets.first(1).each do |secret_key, secrets|
+    all_secrets.each do |secret_key, secrets|
       logger.info "Run for #{secret_key}"
       courier = VirusScanService::Courier.new host: secrets.fetch(:host), token: secrets.fetch(:token)
       courier.logger = logger
@@ -30,15 +31,16 @@ loop do
           .tap { |path| FileUtils.mkdir_p(path) }
 
         kaspersky.antivirus_exec = VirusScanService::KasperskyRunner::WindowsExecutor.new
-        #kaspersky.antivirus_exec = VirusScanService::KasperskyRunner::LinuxExecutor.new
+        # kaspersky.antivirus_exec = VirusScanService::KasperskyRunner::LinuxExecutor.new
 
         kaspersky.call
         kaspersky.result # return result to PUT request (E.g: Clean)
       end
     end
-    sleep 2
+    sleep 2 # sleep between servers
   rescue => e
     logger.info e.to_s
     sleep 60
   end
+  sleep 8 # sleep on new secret cycle
 end
