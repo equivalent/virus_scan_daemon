dfe00d6d6d624c8d4e1aba85c493bafdd90e27bb
Tomas Valent <equivalent@eq8.eu>
2015-03-09 17:53:17 +0000

    remove auto update from daemon

--- daemon.rb
+++ daemon.rb
@@ -1,6 +1,5 @@
 require 'logger'
 errors_count = 0
-counter = 0
 
 logger = Logger.new('daemon.log')
 logger.level = Logger::ERROR
@@ -8,27 +7,7 @@ logger.level = Logger::INFO  if File.exist?('info')
 logger.level = Logger::DEBUG if File.exist?('debug')
 
 loop do
-  counter += 1
-
   begin
-    if counter % 50 == 0
-      logger.info "running git pull"
-      if system('git pull')
-        logger.info("git pull complete")
-      else
-        logger.error("git pull NOT-SUCCESSFUL")
-      end
-
-      logger.info "running bundle update"
-      if system('bundle update')
-        logger.info("update complete")
-      else
-        logger.error("update NOT-SUCCESSFUL")
-      end
-
-      counter = 0
-    end
-
     if system('bundle exec ruby script.rb')
       logger.info("exec ruby script success")
     else
